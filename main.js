da65e98d6937045325ae791f430f524dfcad144e
Nancy Osondu <sophianancy15@gmail.com>
2024-09-05 20:16:43 +0100

    Add game loop

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-alert('Welcome to Quiz Ninja!');
+alert('Welcome to Quiz Ninja!');// introduction to quiz Ninja
 
-const question = "What is Superman's real name?"
-const answer = prompt(question);
-alert(`You answered ${answer}`);
+const quiz = [
+  ["What is Superman's real name?", "Clark Kent"],
+  ["What is Wonder woman's real name?", "Diana Prince"],
+  ["What is Batman's real name?", "Bruce Wayne"] // array of questions
+];
+
+let score = 0 // initial score
+
+for (const [question, answer] of quiz) {
+  const response = prompt(question);
+  if (response === answer) {
+    alert('Correct');
+    score++;
+  } else {
+    alert(`Wrong! The correct answer was ${answer}`)
+  }
+}//game loop
+
+//At the end of the game, report the player's score 
+alert(`Game over, you score ${score} point${score !== 1 ? 's' : ''}`); 
